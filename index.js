f1184f56c4ae0870413ba393dd229b44f6bebf9b
Misho Dzuliashvili <87991798+mishodzuliashvili@users.noreply.github.com>
2023-08-11 12:20:13 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const Joi = require("joi");
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
+];
+
+const validateGenre = (genre) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+};
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+  res.send(genre);
+});
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
